b8a549818391cd739db92232f525174beabf2aa6
Bogdan-67 <88329688+Bogdan-67@users.noreply.github.com>
2023-09-22 01:37:10 +0300

    admin cases menu

--- frontend/src/components/screens/admin/cases/index.tsx
+++ frontend/src/components/screens/admin/cases/index.tsx
@@ -1,13 +1,31 @@
 import AdminLayout from '@/components/AdminLayout';
 import React from 'react';
 import styles from './Cases.module.scss';
+import { useRouter } from 'next/router';
 
 type Props = {};
 
+const LinkItems = [
+  {
+    title: 'Создание кейса',
+    link: '/create',
+  },
+  {
+    title: 'Редактирование кейса',
+    link: '/edit',
+  },
+];
+
 const AdminCases = (props: Props) => {
+  const { pathname, push } = useRouter();
+
   return (
-    <AdminLayout>
-      <h1 className={styles.title}>Выберите действие</h1>
+    <AdminLayout title='Выберите действие'>
+      <div className={styles.buttons}>
+        {LinkItems.map((item) => (
+          <button onClick={() => push(pathname + item.link)}>{item.title}</button>
+        ))}
+      </div>
     </AdminLayout>
   );
 };
