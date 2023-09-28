33e41013a137d933599fac5a7867bf2019d59f67
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-09-29 01:46:49 +0300

    open case page

--- frontend/src/components/screens/cases/CaseScreen/index.tsx
+++ frontend/src/components/screens/cases/CaseScreen/index.tsx
@@ -1,24 +1,81 @@
 import Layout from '@/components/Layout';
 import { ICase } from '@/models/ICase';
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import styles from './CaseScreen.module.scss';
 import StockCard from '@/components/StockCard';
 import { MdOutlineFavoriteBorder, MdFavorite } from 'react-icons/md';
 import { useOpenCaseMutation } from '@/redux/services/case';
 import LoadingSpinner from '@/components/LoadingSpinner/LoadingSpinner';
+import CaseScript from '@/services/caseScript';
+import { IStock } from '@/models/IStock';
+import Image from 'next/image';
+import { API_URL } from '@/http';
+import RewardCard from '@/components/RewardCard';
 
 type Props = {
   icase: ICase;
 };
 
 const CaseScreen: FC<Props> = ({ icase }) => {
-  const [openCase, { data, isSuccess, isLoading, isError, reset }] = useOpenCaseMutation();
+  const [openCase, { reset }] = useOpenCaseMutation();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isError, setIsError] = useState('');
+  const [result, setResult] = useState<Partial<IStock>>({});
+  const [isWaiting, setIsWaiting] = useState(false);
 
   const handleOpenFast = async () => {
-    await openCase({ id_case: icase.id_case });
+    setIsSuccess(false);
+    setIsLoading(true);
+    await openCase({ id_case: icase.id_case, type: 'fast' })
+      .unwrap()
+      .then((response) => {
+        setIsSuccess(true);
+        setResult(response.reward);
+        setIsLoading(false);
+      })
+      .catch((e) => {
+        setIsLoading(false);
+        console.log(e);
+        setIsError('error');
+      });
+  };
+
+  const handleOpenScript = async (fillers: IStock[], reward: IStock) => {
+    setIsWaiting(true);
+
+    setTimeout(async () => {
+      const sound = new Audio('/assets/audio/open-case-sound.mp3');
+      sound.play();
+      await CaseScript.openCase(fillers, reward).then((_) => {
+        setResult(reward);
+        const sound = new Audio('/assets/audio/get-reward.mp3');
+        sound.play();
+        setIsWaiting(false);
+      });
+    }, 100);
+  };
+
+  const handleOpen = async () => {
+    setIsSuccess(false);
+    setIsLoading(true);
+    await openCase({ id_case: icase.id_case, type: 'roll' })
+      .unwrap()
+      .then((response) => {
+        setIsSuccess(true);
+        setIsLoading(false);
+        handleOpenScript(response.fillers, response.reward);
+      })
+      .catch((e) => {
+        setIsLoading(false);
+        console.log(e);
+        setIsError('error');
+      });
   };
 
   const handleRetry = () => {
+    setResult({});
+    setIsSuccess(false);
     reset();
   };
 
@@ -29,32 +86,40 @@ const CaseScreen: FC<Props> = ({ icase }) => {
           {icase.name}
           <MdOutlineFavoriteBorder title={'Добавить в избранное'} className={styles.favorite} />
         </h1>
-        {isLoading ? (
-          <LoadingSpinner />
-        ) : isError ? (
-          <div>{isError ? isError : 'Произошла ошибка :('}</div>
-        ) : isSuccess ? (
-          <>
-            <div className={styles.casePage__content}>
-              <StockCard stock={data} />
+        <div className={styles.unboxArea}>
+          {isLoading ? (
+            <LoadingSpinner />
+          ) : isSuccess && isWaiting ? (
+            <div id='unbox-area'>
+              <div id='cardList'></div>
+              <div className='arrow-down'></div>
+              <div className='gradient-shade gradient-shade_left'></div>
+              <div className='gradient-shade gradient-shade_right'></div>
+              <div></div>
             </div>
-            <div className={styles.casePage__buttons}>
-              <button onClick={handleRetry}>Открыть еще раз</button>
-            </div>
-          </>
-        ) : (
-          <>
-            <div className={styles.casePage__content}></div>
-            <div className={styles.casePage__buttons}>
-              <button>Открыть за {icase.price} руб.</button>
-              <button onClick={handleOpenFast}>Открыть быстро</button>
-            </div>
-          </>
-        )}
+          ) : isSuccess ? (
+            <>
+              <div className={styles.casePage__content}>
+                <RewardCard {...result} />
+              </div>
+              <div className={styles.casePage__buttons}>
+                <button onClick={handleRetry}>Открыть еще раз</button>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className={styles.casePage__content}></div>
+              <div className={styles.casePage__buttons}>
+                <button onClick={handleOpen}>Открыть за {icase.price} руб.</button>
+                <button onClick={handleOpenFast}>Открыть быстро</button>
+              </div>
+            </>
+          )}
+        </div>
         <h2 className={styles.casePage__title_scope}>Содержимое</h2>
         <div className={styles.scope}>
           {icase.items.map((stock) => (
-            <StockCard stock={stock} />
+            <StockCard key={stock.id_stock} stock={stock} />
           ))}
         </div>
       </div>
